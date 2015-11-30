e3e1d86fb8009363e2ddfd32dc4c509f3c55e342
Chris Anderson <chriskenananderson@gmail.com>
2015-11-29 21:41:20 -0600

    added get_ptr methods row_of/col_of/box_of macros

--- pseudoku.c
+++ pseudoku.c
@@ -11,8 +11,9 @@ int32 main()
     print_grid(test_grid);
 }
 
-uint32* get_row(uint32* g, uint32 r)
-{
+uint32*
+get_row(uint32* g, uint32 r)
+{// returns row r (0-8) of grid g
     uint32* row = (uint32*)malloc(sizeof(uint32) * 9);
     uint32 i;
     for (i = 0; i < 9; i++)
@@ -20,8 +21,9 @@ uint32* get_row(uint32* g, uint32 r)
     return row;
 }
 
-uint32* get_col(uint32* g, uint32 c)
-{
+uint32*
+get_col(uint32* g, uint32 c)
+{// returns row c (0-8) of grid g
     uint32* col = (uint32*)malloc(sizeof(uint32) * 9);
     uint32 i;
     for (i = 0; i < 9; i++)
@@ -29,8 +31,9 @@ uint32* get_col(uint32* g, uint32 c)
     return col;
 }
 
-uint32* get_box(uint32* g, uint32 b)
-{
+uint32*
+get_box(uint32* g, uint32 b)
+{// returns row b (0-8) of grid g
     uint32* box = (uint32*)malloc(sizeof(uint32) * 9);
     uint32 start_row = (b / 3) * 3;
     uint32 start_col = (b % 3) * 3;
@@ -41,8 +44,27 @@ uint32* get_box(uint32* g, uint32 b)
     return box;
 }
 
-peers* get_peers(uint32* g, uint32 n)
+uint32*
+get_row_ptr(uint32* g, uint32 r)
 {
+    return &g[r * 9];
+}
+
+uint32*
+get_col_ptr(uint32* g, uint32 c)
+{
+    return &g[c];
+}
+
+uint32*
+get_box_ptr(uint32* g, uint32 b)
+{
+    return &g[(b / 3 * 3 * 9) + (b % 3 * 3)];
+}
+
+peers*
+get_peers(uint32* g, uint32 n)
+{// returns peers of cell n (0-80) in grid g
     peers* neighbors = (peers*)malloc(sizeof(peers));
     uint32* row = get_row(g, n / 9);
     uint32* col = get_col(g, n % 9);
@@ -58,8 +80,9 @@ peers* get_peers(uint32* g, uint32 n)
     return neighbors;
 }
 
-uint32 is_safe(uint32* g, uint32 n, uint32 v)
-{
+uint32
+is_safe(uint32* g, uint32 n, uint32 v)
+{// returns whether or not value v (1-9) in cell n (0-8) in grid g is safe
     peers* neighbors = get_peers(g, n);
     uint32 i;
     for (i = 0; i < 9; i++)
@@ -76,8 +99,9 @@ uint32 is_safe(uint32* g, uint32 n, uint32 v)
     return (1);
 }
 
-pos* get_pos_vals(uint32* g, uint32 n)
-{
+pos*
+get_pos_vals(uint32* g, uint32 n)
+{// returns pos struct containing possible values in cell n (0-80) in grid g
     pos* pos_values = (pos*)malloc(sizeof(pos));
     uint32 i, s;
     for (i = 0, s = 0; i < 9; i++)
@@ -93,8 +117,9 @@ pos* get_pos_vals(uint32* g, uint32 n)
     return pos_values;
 }
 
-uint32 fill_grid(uint32* g, uint32 n)
-{
+uint32
+fill_grid(uint32* g, uint32 n)
+{// fills cells n (0-80) to end of grid g
     if (n < 81)
     {
 	if (g[n] != 0)
@@ -128,8 +153,9 @@ uint32 fill_grid(uint32* g, uint32 n)
     }
 }
 
-void copy_grid(uint32* old_values, uint32* new_values)
-{
+void
+copy_grid(uint32* old_values, uint32* new_values)
+{// copies new_values to old_values in grid g
     uint32 i;
     for (i = 0; i < 81; i++)
     {
@@ -137,8 +163,9 @@ void copy_grid(uint32* old_values, uint32* new_values)
     }
 }
 
-void init_grid(uint32* g)
-{
+void
+init_grid(uint32* g)
+{// initializes grid g to samp_grid
     uint32 samp_grid[81] = {0, 0, 0, 0, 0, 0, 0, 0, 0,
 			    0, 0, 0, 0, 0, 0, 0, 0, 0,
 			    0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -151,8 +178,9 @@ void init_grid(uint32* g)
     copy_grid(g, samp_grid);
 }
 
-void print_grid(uint32* g)
-{
+void
+print_grid(uint32* g)
+{// prints grid g in Sudoku format
     uint32 i;
     for (i = 0; i < 81; i++)
     {
--- pseudoku.h
+++ pseudoku.h
@@ -6,27 +6,35 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define row_of(p) {p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8]}
+#define col_of(p) {p[0], p[9], p[18], p[27], p[36], p[45], p[54], p[63], p[72]}
+#define box_of(p) {p[0], p[1], p[2], p[9], p[10], p[11], p[18], p[19], p[20]}
 
+//typedefs for integer types
 typedef int8_t int8;
 typedef int16_t int16;
 typedef int32_t int32;
 typedef int64_t int64;
-
+//typedefs for unsigned integer types
 typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
-
+//typedefs for floating-point types
 typedef float real32;
 typedef double real64;
+typedef long double real128;
 
-typedef struct peers {
+//contains all peers (row/col/box values) of a cell
+typedef struct
+{
     uint32 row[9];
     uint32 col[9];
     uint32 box[9];
 } peers;
-
-typedef struct pos {
+//contains possible values of a cell w/number of vals
+typedef struct
+{
     uint32 vals[9];
     uint32 size;
 } pos;
@@ -34,6 +42,9 @@ typedef struct pos {
 uint32* get_row(uint32*, uint32);
 uint32* get_col(uint32*, uint32);
 uint32* get_box(uint32*, uint32);
+uint32* get_row_ptr(uint32*, uint32);
+uint32* get_col_ptr(uint32*, uint32);
+uint32* get_box_ptr(uint32*, uint32);
 peers* get_peers(uint32*, uint32);
 uint32 is_safe(uint32*, uint32, uint32);
 pos* get_pos_vals(uint32*, uint32);
